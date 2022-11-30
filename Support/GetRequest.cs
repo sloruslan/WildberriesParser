863c6514db27184b3d1d2ab292fd53e6f0768025
KOMPUTER\Руслан <sloruslan@mail.ru>
2022-11-30 22:55:04 +0300

    2 урок. Добавил классы

--- Support/GetRequest.cs
+++ Support/GetRequest.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace Support
+namespace BlablacarApi
 {
     public class GetRequest
     {
@@ -12,12 +13,9 @@ namespace Support
         string _address;
 
         public Dictionary<string, string> Headers { get; set; }
-
         public string Response { get; set; }
         public string Accept { get; set; }
         public string Host { get; set; }
-        public string Referer { get; set; }
-        public string Useragent { get; set; }
         public WebProxy Proxy { get; set; }
 
         public GetRequest(string address)
@@ -26,47 +24,52 @@ namespace Support
             Headers = new Dictionary<string, string>();
         }
 
-        public void Run()
-        {
-            _request = (HttpWebRequest)WebRequest.Create(_address);
-            _request.Method = "Get";
-
-            try
-            {
-                HttpWebResponse response = (HttpWebResponse)_request.GetResponse();
-                var stream = response.GetResponseStream();
-                if (stream != null) Response = new StreamReader(stream).ReadToEnd();
-            }
-            catch (Exception)
-            {
-            }
-        }
-
         public void Run(CookieContainer cookieContainer)
         {
             _request = (HttpWebRequest)WebRequest.Create(_address);
-            _request.Method = "Get";
+            _request.Method = "GET";
             _request.CookieContainer = cookieContainer;
             _request.Proxy = Proxy;
             _request.Accept = Accept;
             _request.Host = Host;
-            _request.Referer = Referer;
-            _request.UserAgent = Useragent;
 
             foreach (var pair in Headers)
             {
                 _request.Headers.Add(pair.Key, pair.Value);
             }
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)_request.GetResponse();
+                var stream = response.GetResponseStream();
+                if (stream != null)
+                {
+                    Response = new StreamReader(stream).ReadToEnd();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        public void Run()
+        {
+            _request = (HttpWebRequest)WebRequest.Create(_address);
+            _request.Method = "GET";
 
             try
             {
                 HttpWebResponse response = (HttpWebResponse)_request.GetResponse();
                 var stream = response.GetResponseStream();
-                if (stream != null) Response = new StreamReader(stream).ReadToEnd();
+                if (stream != null)
+                {
+                    Response = new StreamReader(stream).ReadToEnd();
+                }
             }
             catch (Exception)
             {
-            }
+
+            } 
         }
     }
 }
--- Support/PostRequest.cs
+++ Support/PostRequest.cs
@@ -5,23 +5,22 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp.Services
+namespace BlablacarApi
 {
-    public class GetRequest
+    public class PostRequest
     {
         HttpWebRequest _request;
         string _address;
 
         public Dictionary<string, string> Headers { get; set; }
-
         public string Response { get; set; }
         public string Accept { get; set; }
         public string Host { get; set; }
-        public string Referer { get; set; }
-        public string Useragent { get; set; }
+        public string Data { get; set; }
+        public string ContentType { get; set; }
         public WebProxy Proxy { get; set; }
 
-        public GetRequest(string address)
+        public PostRequest(string address)
         {
             _address = address;
             Headers = new Dictionary<string, string>();
@@ -30,28 +29,57 @@ namespace ConsoleApp.Services
         public void Run(CookieContainer cookieContainer)
         {
             _request = (HttpWebRequest)WebRequest.Create(_address);
-            _request.Method = "Get";
+            _request.Method = "POST";
             _request.CookieContainer = cookieContainer;
             _request.Proxy = Proxy;
             _request.Accept = Accept;
             _request.Host = Host;
-            _request.Referer = Referer;
-            _request.UserAgent = Useragent;
+            _request.ContentType = ContentType;
+
+
+            byte[] sentData = Encoding.UTF8.GetBytes(Data);
+            _request.ContentLength = sentData.Length;
+            Stream sendStream= _request.GetRequestStream();
+            sendStream.Write(sentData, 0, sentData.Length);
+            sendStream.Close();
 
             foreach (var pair in Headers)
             {
                 _request.Headers.Add(pair.Key, pair.Value);
             }
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)_request.GetResponse();
+                var stream = response.GetResponseStream();
+                if (stream != null)
+                {
+                    Response = new StreamReader(stream).ReadToEnd();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        public void Run()
+        {
+            _request = (HttpWebRequest)WebRequest.Create(_address);
+            _request.Method = "POST";
 
             try
             {
                 HttpWebResponse response = (HttpWebResponse)_request.GetResponse();
                 var stream = response.GetResponseStream();
-                if (stream != null) Response = new StreamReader(stream).ReadToEnd();
+                if (stream != null)
+                {
+                    Response = new StreamReader(stream).ReadToEnd();
+                }
             }
             catch (Exception)
             {
-            }
+
+            } 
         }
     }
 }
